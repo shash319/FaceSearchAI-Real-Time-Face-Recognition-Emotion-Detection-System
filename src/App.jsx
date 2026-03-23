b48ea7501c1833f56201e71f419d5f72328438fd
Shasawat Pal <shashwatpal2001@gmail.com>
2026-03-23 13:45:27 +0530

    AI Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,48 @@
-import React from 'react'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Navbar from './Compo/Navbar' 
-import About from './pages/About';
-import Register from './Compo/Register';
-import Features from './pages/Features';
-import Tokens from './pages/Tokens';
-import Contact from './pages/Contact';
+import React from "react";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
+
+import Navbar from "./Compo/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Features from "./pages/Features";
+import Tokens from "./pages/Tokens";
+import Contact from "./pages/Contact";
 import Login from "./Compo/Login";
-import Home from "./Compo/Navbar";
+import Register from "./Compo/Register";
 import VIP from "./Compo/Vip";
 import Face from "./Compo/Face";
 
-const router = createBrowserRouter(
-  [
-    {
-        path: '/',
-        element: <Navbar/>
-    },
-    {
-        path: '/',
-        element: <Home/>
-    },
-    {
-        path: "/vip",
-        element: <VIP/>
-    },
-     {
-        path: "/faceai",
-        element: <Face/>
-    },
-     {
-        path: '/about',
-        element: <About/>
-    },
-     {
-        path: '/features',
-        element: <Features/>
-    },
-     {
-        path: '/tokens',
-        element: <Tokens/>
-    },
-     {
-        path: '/contact',
-        element: <Contact/>
-    },
-    {
-        path: '/login',
-        element: <Login/>
-    },
-    {
-        path: '/register',
-        element: <Register/>
-    }
-    
-  ]
-)
+const Layout = ()=>{
+  const location = useLocation();
+  return (
+    <>
+    {location.pathname === '/' && <Navbar/>}
+
+ <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vip" element={<VIP />} />
+          <Route path="/faceai" element={<Face />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/tokens" element={<Tokens />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+
+    </>
+  );
+};
+
 
 const App = () => {
   return (
-    <div>
-      <RouterProvider router={router}/>
-    </div>
-  )
-}
+    <HashRouter>
 
-export default App
+    <Layout/>
+
+    </HashRouter>
+  );
+};
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   base: "/"
-});
+  plugins: [react()],
+  base: "/FaceSearchAI-Real-Time-Face-Recognition-Emotion-Detection-System/"
+})
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//    plugins: [react()],
+//    base: "/"
+// });
